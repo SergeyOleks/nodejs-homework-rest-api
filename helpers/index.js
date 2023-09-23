2cc575b26b38c4193d92c1feca33b2b6f888440f
SergeyOleks <oleksienkosa@gmail.com>
2023-09-23 11:18:46 +0300

    create

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,11 @@
 const HttpError = require("./HttError");
 const handleMongooseError = require("./handleMongooseError");
 const ctrlWrapper = require("./ctrlWrapper");
-
+const sendEmail = require("./sendEmail");
 
 module.exports = {
   HttpError,
   handleMongooseError,
-  ctrlWrapper
+  ctrlWrapper,
+  sendEmail,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,6 +9,10 @@ const { schemas } = require("../../models/user");
 //signup
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
+router.get("/verify/:verificationCode", ctrl.verifyEmail);
+
+router.post("/verify", validateBody(schemas.emailSchema), ctrl.resendVerifyEmail);
+
 //signin
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
@@ -22,7 +26,7 @@ router.post("/logout", authenticate, ctrl.logout);
 router.patch(
   "/avatars",
   authenticate,
-  upload.single('avatar'),
+  upload.single("avatar"),
   ctrl.updateAvatar
 );
 
